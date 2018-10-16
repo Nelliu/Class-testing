8b57748093de87fe0db7c39e6ff023b76a2047a6
matyas.kloub <matyas.kloub@seznam.cz>
2018-10-16 09:13:10 +0200

    Genders List, counting

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Bdate { get; set; }
+        public string Gender { get; set; }
+
+        public bool Alive { get; set; }
+
+
+        List<string> Names = new List<string>() { "Honza", "Pepa", "Kuba", "Franta", "Jirka" };
+        List<string> LastNames = new List<string>() { "Nový", "Novotný", "Starý", "Modrý", "Novák" };
+        List<int> Bdates = new List<int>() { 1799, 2000, 1997, 2000, 1996 };
+        List<string> Genders = new List<string>() { "Man", "Woman" };
+
+
+
+
+        Random randomn = new Random();
+
+        
+        
+        public Person Addto(int rand, int rand1)
+        {
+            bool isalive = false;
+            if (rand1 == 0)
+            {
+                isalive = true;
+            }
+            
+
+            Person returningP = new Person();
+            returningP.Name = Names[rand];
+            returningP.LastName = LastNames[rand];
+            returningP.Bdate = Bdates[rand];
+            returningP.Gender = Genders[0];
+            returningP.Alive = isalive;
+
+
+            return returningP;
+        }
+
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,87 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<Person> People = new List<Person>();
+
+            Person Person1 = new Person();
+            Person Person2 = new Person();
+            Person Person3 = new Person();
+            Person Person4 = new Person();
+            Person Person5 = new Person();
+
+            Person addon = new Person();
+            
+
+            
+            
+
+            Person1 = Person1.Addto(0,0);
+            Person2 = Person1.Addto(1, 1);
+            Person3 = Person1.Addto(2, 0);
+            Person4 = Person1.Addto(3, 1);
+            Person5 = Person1.Addto(4, 1);
+
+            People.Add(Person1);
+            People.Add(Person2);
+            People.Add(Person3);
+            People.Add(Person4);
+            People.Add(Person5);
+
+            int CurrentYear = 2018;
+            int howm = 0;
+            int AgeofAll = 0;
+            int men = 0;
+            int woman = 0;
+            int other = 0;
+
+            for (int i = 0; i < People.Count; i++)
+            {
+                Console.WriteLine("Name: " + People[i].Name);
+                Console.WriteLine("Last name: " + People[i].LastName);
+                Console.WriteLine("Year of birth: " + People[i].Bdate);
+                Console.WriteLine("Gender: " + People[i].Gender);
+                
+                if (People[i].Alive == true)
+                {
+                    Console.WriteLine("Status: Alive");
+                }
+                else
+                {
+                    Console.WriteLine("Status: Dead");
+                }
+                Console.WriteLine();
+
+                AgeofAll = AgeofAll + (CurrentYear - People[i].Bdate);
+                howm++;
+
+                if (People[i].Gender == "Man")
+                {
+                    men++;
+                }
+                else if (People[i].Gender == "Woman")
+                {
+                    woman++;
+                }
+                else
+                {
+                    other++;
+                }
+
+            }
+
+            Console.WriteLine("Average age: "+(AgeofAll / howm));
+            Console.WriteLine("Men: " + men);
+            Console.WriteLine("Women: " + woman);
+            Console.WriteLine("Other: " + other);
+
+            
+
+            
+
+
+
+
+
         }
     }
 }
